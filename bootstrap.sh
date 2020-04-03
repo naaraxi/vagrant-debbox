c40c40004db4a00e7ca36e9ce731abf28b8b0678
Alex Mazalu <george@nordicwebteam.se>
2020-04-03 12:27:50 +0300

    Changes to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 # Using single quotes instead of double quotes to make it work with special-character passwords
-PHP_VERSION='7.2'
-PROJECT_GIT='[[PROJECT_GIT]]'
-PROJECT_NAME='[[PROJECT_NAME]]'
+PHP_VERSION='(PHP.VERSION)'
+PROJECT_NAME='(PROJECT_NAME)'
 
 PASSWORD='vagrant'
 PROJECTFOLDER='html'
 
 # update / upgrade
 echo "Updating apt-get..."
-apt-get update
-apt-get -y upgrade
+apt-get update > /dev/null 2>&1
+apt-get -y upgrade > /dev/null 2>&1
 
-apt-get install -y software-properties-common
+apt-get install -y software-properties-common > /dev/null 2>&1
 
 # install git
 echo "Installing Git..."
-apt-get install -y git
+apt-get install -y git > /dev/null 2>&1
 
 # install nginx
 echo "Installing Nginx..."
-LC_ALL=C.UTF-8 apt-get install -y nginx nginx-full nginx-common
+LC_ALL=C.UTF-8 apt-get install -y nginx nginx-full nginx-common > /dev/null 2>&1
 
 #install gpg
 apt-get install -y gnupg > /dev/null 2>&1
@@ -29,46 +28,46 @@ apt-get install -y gnupg > /dev/null 2>&1
 # install php7-fpm
 echo "Installing PHP..."
 
-apt-get install -y apt-transport-https lsb-release ca-certificates
+apt-get install -y apt-transport-https lsb-release ca-certificates > /dev/null 2>&1
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
-apt update
+apt update > /dev/null 2>&1
 
-apt-get install -y php${PHP_VERSION}-mysql
-apt-get install -y php${PHP_VERSION}-common
-# apt-get install -y php${PHP_VERSION}-mcrypt
-apt-get install -y php${PHP_VERSION}-zip
-apt-get install -y php${PHP_VERSION}-pdo
-apt-get install -y php${PHP_VERSION}-mysqlnd
-apt-get install -y php${PHP_VERSION}-opcache
-apt-get install -y php${PHP_VERSION}-xml
-apt-get install -y php${PHP_VERSION}-xdebug
-apt-get install -y php${PHP_VERSION}-gd
-apt-get install -y php${PHP_VERSION}-mysql
-apt-get install -y php${PHP_VERSION}-intl
-apt-get install -y php${PHP_VERSION}-mbstring
-apt-get install -y php${PHP_VERSION}-bcmath
-apt-get install -y php${PHP_VERSION}-json
-apt-get install -y php${PHP_VERSION}-iconv
-apt-get install -y php${PHP_VERSION}-soap
-apt-get install -y php${PHP_VERSION}-fpm
+apt-get install -y php${PHP_VERSION}-mysql > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-common > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-mcrypt > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-zip > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-pdo > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-mysqlnd > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-opcache > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-xml > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-xdebug > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-gd > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-mysql > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-intl > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-mbstring > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-bcmath > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-json > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-iconv > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-soap > /dev/null 2>&1
+apt-get install -y php${PHP_VERSION}-fpm > /dev/null 2>&1
 
 # install mariadb and give password to installer
 echo "Preparing MariaDB..."
-apt-get install -y debconf-utils
+apt-get install -y debconf-utils > /dev/null 2>&1
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 
 echo "Updating apt-get..."
-apt-get update
- 
+apt-get update > /dev/null 2>&1
+
 echo "Installing MariaDB..."
-apt-get install -y mariadb-server
+apt-get install -y mariadb-server > /dev/null 2>&1
 
 echo "Installing composer..."
-apt-get -y install composer
+apt-get -y install composer > /dev/null 2>&1
 
-apt-get -y --fix-broken install
+apt-get -y --fix-broken install > /dev/null 2>&1
 
 # Nginx Config
 echo "Configuring Nginx..."
@@ -79,21 +78,9 @@ rm -rf /etc/nginx/sites-enabled/default
 
 # Restarting Nginx for config to take effect
 echo "Restarting Nginx..."
-service nginx restart
+service nginx restart > /dev/null 2>&1
+rm /var/www/html/index.nginx-debian.html
 
-# Set up composer auth, ssh keys
-mkdir /home/vagrant/.composer/
-cp /var/www/config/id_rsa /home/vagrant/.ssh/id_rsa
-cp /var/www/config/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
-cp /var/www/config/auth.json /home/vagrant/.composer/auth.json
-
-# Import project
+# Import database
 mysql -u root -pvagrant -e "CREATE DATABASE IF NOT EXISTS ${PROJECT_NAME} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-
-eval $(ssh-agent)
-ssh-add /home/vagrant/.ssh/id_rsa
-ssh-keyscan bitbucket.org > /home/vagrant/.ssh/known_hosts
-
-cd /var/www/html/ && rm -rf ./* && git clone ${PROJECT_GIT} .
-composer install --working-dir="/var/www/html/"
 zcat /var/www/config/database.sql.gz | mysql -u root -pvagrant ${PROJECT_NAME}
